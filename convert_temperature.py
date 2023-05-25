50ef622b51883ebbd8b570bc1233836120a83aef
Paige Folsom <folsom.paige@gmail.com>
2023-05-25 14:25:31 +0200

    this will give... a docstring for the source code documentation

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,4 +1,13 @@
 def fahrenheit_to_celcius(temperature_Fahrenheit):
+	"""
+	Converts temperature in fahrenheit to celcius
+
+	Parameters: 
+	temperature_fahrenheit
+
+	Returns: 
+	temperature_celcius
+	"""
 	temperature_/Celcius = (temp_f- 32) * (5.0/9.0)
 	return temp_Celcius 
 
