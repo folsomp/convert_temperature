b7d8d286ecdeaafbeb6b4811fa166a26dc7510a4
Paige Folsom <folsom.paige@gmail.com>
2023-05-25 14:09:15 +0200

    convert_temperature.py

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,4 +1,4 @@
-def fahrenheit_to_celcius(temp_fahrenheit):
-	temp_celcius = (temp_f- 32) * (5.0/9.0)
-	return temp_celcius 
+def fahrenheit_to_celcius(temperature_Fahrenheit):
+	temperature_/Celcius = (temp_f- 32) * (5.0/9.0)
+	return temp_Celcius 
 
